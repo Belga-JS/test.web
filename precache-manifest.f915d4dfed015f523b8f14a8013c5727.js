25865e7f382107e6f810b1d587a42405dfa33175
Belga-JS <Meherzi.belga@yahoo.com>
2019-12-30 17:28:32 +0100

    Updates

---
+++ precache-manifest.f915d4dfed015f523b8f14a8013c5727.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a461039f16d58f92cd86871ff9d123",
+    "url": "/test.web/index.html"
+  },
+  {
+    "revision": "ad0714d075f85db6bfb5",
+    "url": "/test.web/static/css/main.f8b90492.chunk.css"
+  },
+  {
+    "revision": "b7a9a66c610ecb3b466c",
+    "url": "/test.web/static/js/2.95484bb0.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/test.web/static/js/2.95484bb0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ad0714d075f85db6bfb5",
+    "url": "/test.web/static/js/main.a4bf1c5c.chunk.js"
+  },
+  {
+    "revision": "1a4e3ef64b62e5caf313",
+    "url": "/test.web/static/js/runtime-main.a97b6f17.js"
+  }
+]);
